3ebc7c1cd06142a54901a3bfbe0651f25666b7b8
Camille Copeland <camille.copeland@accenture.com>
2022-11-06 17:10:45 -0600

    set up data context

---
+++ Data/DataContext.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace dot_net.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Character> Characters => Set<Character>();
+    }
+}
